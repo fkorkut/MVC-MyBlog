6ab9780f6bb3e622525585dbf78e533ccfd9c1d2
fkorkut <frdvskrkt@gmail.com>
2019-06-26 16:19:00 +0300

    first commit

---
+++ MyBlog.Panel/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Panel.Controllers
+{
+    public class UserController : Controller
+    {
+        //List: FullName, CityName, TownName, EmailAddress,UserTypeName, Detay
+        //Save
+        //IsEmailVerified=true
+        //UserTypeID=2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
